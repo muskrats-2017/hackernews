2883a558aa12447203309a79787b473c85edf8ea
Thomas Harvey <thomasharvey24@gmail.com>
2017-09-06 12:28:27 -0400

    refactored version

--- project/blog/static/blog/js/main.refactor.js
+++ project/blog/static/blog/js/main.refactor.js
@@ -2,13 +2,23 @@
 //Code taken from Billy's demo on Friday  
 //with attempts to reformat for Hackernews
 
-
-var createForm = function createForm(options){
+var createAJAXForm = function createAJAXForm(options){
+	options = options || {};
 	var self = {};
 
 	self.getForm = options.getForm || function(){};
 
-	self.onSubmit = options.onSubmit || function(){};
+	self.onSubmit = options.onSubmit || function onSubmit($form, onSuccess, onError){
+		var self = this;
+
+		$.ajax({
+			url: $form.attr("action"),
+			method: $form.attr("method"),
+			data: $form.serialize(),
+			success: onSuccess || self.onSuccess,
+			error: onError || self.onError
+		});
+	};
 
 	self.onSuccess = options.onSuccess || function(){};
 
@@ -22,7 +32,7 @@ $(document).ready(function(){
 	
 	$commentTree = $('.comment-tree');
 
-	createCommentForm = createForm({
+	createCommentForm = createAJAXForm({
 		"getForm": function(action){
 			var 
 				template = $("#comment-reply-template").html(),
@@ -32,21 +42,11 @@ $(document).ready(function(){
 				$el.attr("action", action);
 			}
 			return $el;
-		},
-		"onSubmit": function($form, onSuccess, onError){
-			var self = this;
-
-			$.ajax({
-				url: $form.attr("action"),
-				method: $form.attr("method"),
-				data: $form.serialize(),
-				success: onSuccess || self.onSuccess,
-				error: onError || self.onError
-			});
 		}
 	});
 
 	$commentTree.on("click", "a.comment-reply-anchor", function(event){
+		event.preventDefault();
 		var $this = $(this);
 		var $commentFormContainer = $this.parent().siblings(".comment-form-container");
 		var $form = createCommentForm.getForm($this.attr("href"));
@@ -63,7 +63,6 @@ $(document).ready(function(){
 			function(data){
 				var commentFormContainer = $form.parents(".comment-form-container");
 				commentFormContainer.empty();
-				createCommentForm.$el = null;
 				commentFormContainer.siblings("ul").append("<li>" + data + "</li>");
 			}, 
 			function(jqXHR, textStatus, errorThrown){
@@ -72,7 +71,7 @@ $(document).ready(function(){
 		);
 	});
 
-	updateCommentForm = createForm({
+	updateCommentForm = createAJAXForm({
 		"getForm": function(action){
 			var 
 				template = $("#comment-update-template").html(),
@@ -91,24 +90,16 @@ $(document).ready(function(){
 			});
 
 			return $el;
-		},
-		"onSubmit": function($form, onSuccess, onError){
-			var self = this;
-
-			$.ajax({
-				url: $form.attr("action"),
-				method: $form.attr("method"),
-				data: $form.serialize(),
-				success: onSuccess || self.onSuccess,
-				error: onError || self.onError
-			});
 		}
 	});
 
 	$commentTree.on("click", "a.comment-update-anchor", function(event){
+		event.preventDefault();
 		var $this = $(this);
 		var $commentFormContainer = $this.parent().siblings(".comment-form-container");
+		
 		var $form = updateCommentForm.getForm($this.attr("href"));
+		
 		$commentFormContainer.html($form);
 	});
 
@@ -127,19 +118,7 @@ $(document).ready(function(){
 		);
 	});
 	
-	deleteCommentForm = createForm({
-		"onSubmit": function($form, onSuccess, onError){
-			var self = this;
-
-			$.ajax({
-				url: $form.attr("action"),
-				method: $form.attr("method"),
-				data: $form.serialize(),
-				success: onSuccess || self.onSuccess,
-				error: onError || self.onError
-			});
-		}
-	});
+	deleteCommentForm = createAJAXForm();
 
 	$commentTree.on("submit", ".comment form.comment-delete-form", function(event){
 		event.preventDefault();
